285f70674274bf155881e5403c0e5bab55512f62
Alina Tsui <7786266+ALINATSUI@users.noreply.github.com>
2026-06-12 20:18:44 -0400

    Removed old query1.csv; Updated streamlit_bq.py

--- streamlit_bq.py
+++ streamlit_bq.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit as st, pandas as pd
 from google.oauth2 import service_account
 from google.cloud import bigquery
 credentials = service_account.Credentials.from_service_account_info(
@@ -12,8 +12,17 @@ def run_query(query):
     rows = [dict(row) for row in rows_raw]
     return rows
 
-rows = run_query("SELECT name,brand,category, retail_price FROM `bigquery-public-data.thelook_ecommerce.products` LIMIT 15")
+rows = run_query("SELECT name FROM `bigquery-public-data.thelook_ecommerce.products` LIMIT 15")
 
 st.write("Question 1: Catalog Snapshot")
-for row in rows:
-    st.write(row['name'] + row['brand'] + row['category'] + row['retail_price'])
+# for row in rows:
+    # st.write(row['name'] + row['brand'] + row['category'] + row['retail_price'])
+    # st.write(row['name'])
+# col1, col2, col3, col4 = st.columns(4)
+# for row in rows:
+#         # st.write(row['name'])
+#         st.write(row['brand'])
+f = pd.read_csv("query1.csv")
+query1_df = pd.DataFrame(f)
+st.dataframe(query1_df)
+
