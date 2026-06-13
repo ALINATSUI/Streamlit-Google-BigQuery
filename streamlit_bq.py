39a2c5e42bf016eaa354906375afd04206db8a81
Alina Tsui <7786266+ALINATSUI@users.noreply.github.com>
2026-06-13 13:44:13 -0400

    Added csv output files

--- streamlit_bq.py
+++ streamlit_bq.py
@@ -12,13 +12,20 @@ def run_query(query):
     rows = [dict(row) for row in rows_raw]
     return rows
 
-rows = run_query("SELECT name FROM `bigquery-public-data.thelook_ecommerce.products` LIMIT 15")
+rows = run_query("SELECT name,brand,retail_price,category FROM `bigquery-public-data.thelook_ecommerce.products` LIMIT 15")
 st.title('TLab: SQL Project')
 st.write("Question 1: Catalog Snapshot")
 
-f = pd.read_csv("query.csv")
-query1_df = pd.DataFrame(f)
-st.dataframe(query1_df, height='content')
+f_1 = pd.read_csv("q1.csv")
+f1_df = pd.DataFrame(f_1)
+f1_df.rename(columns={
+    'retail_price': 'RETAIL_PRICE($)',
+    'name': 'PRODUCT NAME',
+    'brand': 'BRAND',
+    'category': 'CATEGORY',
+    
+}, inplace=True)
+st.dataframe(f1_df, height='content')
 
 code = '''def run_query(query):
     query_job = client.query(query)
@@ -45,7 +52,17 @@ q2 = st.container(
 q2.header('Question 2: Premium Products')
 q2.subheader(body = '''
          Finance wants a list of all products with a retail_price greater than $200. Show the product name, brand, and retail_price, sorted from most expensive to least expensive.''', anchor=False)
-# rows2 = run_query("SELECT ")    
+rows2 = run_query("SELECT name, brand, retail_price FROM `bigquery-public-data.thelook_ecommerce.products`")   
+
+f2 = pd.read_csv('q2.csv')
+f2_df = pd.DataFrame(f2)
+f2_df.rename(columns={
+    'retail_price' : 'RETAIL PRICE($)',
+    'name' : 'PRODUCT NAME', 
+    'brand' : 'BRAND'
+}, inplace=True)
+st.dataframe(f2_df, height='content')
+st.divider()
 
 q3 = st.container(
     width='stretch', 
@@ -54,17 +71,29 @@ q3 = st.container(
     border=True, 
     )
 q3.header('Question 3: Profit Margin Calculation')
-q3.subheader(body='''How many products does TheLook carry in each category? Show the category name and the total count, sorted from largest category to smallest.
-💡 Tip: Group your rows by category and count what falls inside each group.
+q3.subheader(body='''
+Calculate the gross profit margin for each product. Gross profit margin is defined as the difference between retail_price and cost. Name this new column profit_margin. Show the product name, retail_price, cost, and profit_margin, sorted by highest margin first.
+💡 Tip: You can subtract one column from another directly inside SELECT, then give the result a clean name using AS.
+
 
 ''', anchor=False)
-
-
+row3 = run_query("SELECT cost,retail_price, name, retail_price-cost AS profit_margin " \
+"FROM `bigquery-public-data.thelook_ecommerce.products`ORDER BY profit_margin DESC")
+f3 = pd.read_csv('q3.csv')
+f3_df = pd.DataFrame(f3)
+f3_df.rename(columns={
+    'cost': 'COST($)',
+    'retail_price': 'RETAIL_PRICE($)',
+    'profit_margin': 'PROFIT_MARGIN($)',
+    'name': 'PRODUCT NAME'
+}, inplace=True)
+st.dataframe(f3_df)
 st.divider(width='stretch')
-q4 = st.container(width='stretch', height='content', autoscroll=True, border=True)
-q4.header('Question 4: Category Inventory Count')
-q4.subheader(body='''How many products does TheLook carry in each category? Show the category name and the total count, sorted from largest category to smallest.
-💡 Tip: Group your rows by category and count what falls inside each group.
-''', anchor=False)
+
+# q4 = st.container(width='stretch', height='content', autoscroll=True, border=True)
+# q4.header('Question 4: Category Inventory Count')
+# q4.subheader(body='''How many products does TheLook carry in each category? Show the category name and the total count, sorted from largest category to smallest.
+# 💡 Tip: Group your rows by category and count what falls inside each group.
+# ''', anchor=False)
 
 
