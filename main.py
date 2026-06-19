d20b4184df1c7e274536591e7151154a0b1595c5
Alina Tsui <7786266+ALINATSUI@users.noreply.github.com>
2026-06-19 11:47:59 -0400

    Adding in Selenium script

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import TimeoutException
 
 
 STREAMLIT_URL = "https://bq--catalog.streamlit.app/"
